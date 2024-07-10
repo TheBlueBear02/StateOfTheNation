4210ca0ff9c1ece0011ba73e25706f535c51ea8d
Amir <amir.gabbay@gmail.com>
2024-07-10 23:09:35 +0200

    added nav system

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,8 +8,16 @@ def get_date(): # return today's date
     return datetime.today().strftime('%d.%m.%Y')
 
 @app.route('/')
-def home():
-    return render_template('offices.html', today_date=get_date(), number=3)
+def index():
+    return render_template('index.html', today_date=get_date())
+
+@app.route('/offices')
+def offices():
+    return render_template('offices.html', today_date=get_date())
+
+@app.route('/demography')
+def demography():
+    return render_template('demography.html', today_date=get_date())
 
 if __name__ == '__main__':
     app.run(debug=True)
