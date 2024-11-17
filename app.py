79434cbd4b14a2d583096f3047ee76dd0fde1671
Amir <amir.gabbay@gmail.com>
2024-11-17 13:10:05 +0200

    pycache

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
+import os
 from flask import Flask
 from flask_migrate import Migrate
 from models import db
 from routes import routes  # Import the routes Blueprint
-import os
 
-app = Flask(__name__)
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'instance', 'sn.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable track modifications
-
-# Initialize dSb with the Flask app
-db.init_app(app)
-
-migrate = Migrate(app, db)  # This line initializes Flask-Migrate
-
-# Register the Blueprint with the app
-app.register_blueprint(routes)
-
-if __name__ == '__main__':
-    app.run("0.0.0.0", debug=True)
+# Create the app factory
+def create_app(config_class=None):
+    app = Flask(__name__)
+    
+    # Set default configuration or override it with a provided config class
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'instance', 'sn.db')}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable track modifications
+    
+    if config_class:
+        app.config.from_object(config_class)
+    
+    # Initialize extensions
+    db.init_app(app)
+    Migrate(app, db)
+    
+    # Register Blueprints
+    app.register_blueprint(routes)
+    
+    return app
