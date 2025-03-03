7db922637c04e6a788f6d61369f7931683e62c19
Amir1 <amir.gabbai8@gmail.com>
2025-03-04 00:51:00 +0200

    fixing design bugs

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app  # Import the factory function
 app = create_app()  # Create the app instance
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", debug=True)
+    app.run("0.0.0.0", debug=True, port=5000)
