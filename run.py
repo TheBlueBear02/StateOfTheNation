c0e5516b059c3dfd70c891bf893253e3401f3140
Amir <amir.gabbay@gmail.com>
2025-05-31 17:14:07 +0300

    added google analytics tags

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app  # Import the factory function
 app = create_app()  # Create the app instance
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5000, debug=True)
